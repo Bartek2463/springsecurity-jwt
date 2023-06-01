841802a837aa9270360f1854c0ea61a8f1a7d757
Bartlomiej <szybiakbartlomiej@gmail.com>
2023-06-01 21:56:23 +0200

    Set admin roles and set filter for ovveride method

---
+++ src/main/java/pl/szybiak/springsecurityjwt/JwtFilter.java
@@ -0,0 +1,31 @@
+package pl.szybiak.springsecurityjwt;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends BasicAuthenticationFilter {
+    public JwtFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilterInternal(request, response, chain);
+        String header = request.getHeader("Authorization");
+
+        UsernamePasswordAuthenticationToken authResult = getAuthenticationEntryPoint()
+
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+
+
+    }
+}
--- src/main/java/pl/szybiak/springsecurityjwt/WebSecurityConfig.java
+++ src/main/java/pl/szybiak/springsecurityjwt/WebSecurityConfig.java
@@ -4,33 +4,34 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(User.builder()
-                .username("test")
-                .password(getPasswordEncoder().encode("test"))
-                .roles("ADMIN"));
-
-    }
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser(User.builder()
+//                .username("test")
+//                .password(getPasswordEncoder().encode("test"))
+//                .roles("ADMIN"));
+//
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/test2").authenticated()
                 .antMatchers("/test3").hasRole("ADMIN")
                 .and()
-                .formLogin().permitAll();
+                .addFilter(new JwtFilter(authenticationManager()));
     }
 }
